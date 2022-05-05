6d20cf0a3ad7f22e6dd7ae3b3b3cb83919c2dfbe
elvysferrari <elvys.ferrari@gmail.com>
2022-05-04 23:29:30 -0300

    ajustes url

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,3 +1,3 @@
-const String empty_user_image = "https://firebasestorage.googleapis.com/v0/b/relatoseqm.appspot.com/o/profile%2Fempty_image_profile.png?alt=media&token=012794fd-6d71-49f8-9a64-75663a4400e3";
-const String baseUrl = 'http://ticke-recip-6qdtpkzxwora-1439999089.sa-east-1.elb.amazonaws.com/api';
+const String empty_user_image = "";
+const String baseUrl = 'http://191.252.157.100:5000/api';
 const String appName = "Ticket";
